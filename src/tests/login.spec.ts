b8b953e7a63f76125a83b345be0e2dac600ac9ec
rushikeshwasule <rushikeshwasule1997@gmail.com>
2026-04-27 10:42:10 +0530

    6th code push

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -6,7 +6,7 @@ import { TestConfig } from '../testData/test.config'
 test('Login Test using POM', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
-  await loginPage.goto();
+  await page.goto(TestConfig.appUrl);
   await loginPage.enterUsername(TestConfig.username);
   await loginPage.enterPassword(TestConfig.password);
   await loginPage.clickLogin();
