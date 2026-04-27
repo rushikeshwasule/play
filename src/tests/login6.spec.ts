4044cb9d61ddad21399bed2c91d465e8e4f37d73
rushikeshwasule <rushikeshwasule1997@gmail.com>
2026-04-27 10:37:21 +0530

    5th code push

--- src/tests/login6.spec.ts
+++ src/tests/login6.spec.ts
@@ -3,7 +3,7 @@ import LoginPage6 from "../page/LoginPage6";
 import { TestConfig6 } from "../testData/test.config6";
 
 
-test.only('Login with valid usernaem and password', async ({page})=>{
+test('Login with valid usernaem and password', async ({page})=>{
 const loginPage6 = new LoginPage6(page);
 await page.goto(TestConfig6.appURL6);
 await loginPage6.fillUsername6(TestConfig6.username6);
